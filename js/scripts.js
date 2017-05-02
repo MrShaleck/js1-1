741ab16f56a40ae1c7dce9fb8a2acf70e2ead1b6
Marcin <marcinrszalek@gmail.com>
2017-05-02 15:49:13 +0200

    pierwszy

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var name = prompt('Wpisz swoje imiê');
+// alert('Witaj, ' + name);
+// wy³¹czam funkcjê alert powy¿ej
+/*
+komentarz blokowy
+*/
+/*
+wyœwietlanie w konsoli
+console.log('Witaj, ' + name);
+*/
